263842d273a3f64b00672e0a84877c059328a148
umberto <umberto.gagliardini@studenti.unicampania.it>
2019-03-13 16:55:39 +0100

    commit iniziale

---
+++ app/src/main/java/com/example/calcolatrice/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.calcolatrice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        somma.setOnClickListener{
+            val a=n1.text.toString().toInt()
+            val b=n2.text.toString().toInt()
+            ris.setText((a+b).toString())
+        }
+
+
+    }
+}
